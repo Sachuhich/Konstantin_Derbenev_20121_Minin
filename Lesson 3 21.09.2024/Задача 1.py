39f4ec8c9a7edca5663e126865227b90afcafc24
Константин <89351929+Sachuhich@users.noreply.github.com>
2024-09-27 23:13:47 +0300

    Update Задача 1.py

--- Lesson 3 21.09.2024/Задача 1.py
+++ Lesson 3 21.09.2024/Задача 1.py
@@ -1,6 +1,4 @@
 num=int(input())
 mun=str(num)[int(len(str(num)))//2::1]
-if len(str(num))%2==0:
-    print('True')if int(str(num)[0:int(len(str(num)))//2])==int(mun[::-1]) else print('False')
-else:
-    print('True')if int(str(num)[0:int(len(str(num)))//2+1])==int(mun[::-1])else print('False')
+if len(str(num))%2==0:print('True')if int(str(num)[0:int(len(str(num)))//2])==int(mun[::-1]) else print('False')
+else:print('True')if int(str(num)[0:int(len(str(num)))//2+1])==int(mun[::-1])else print('False')
