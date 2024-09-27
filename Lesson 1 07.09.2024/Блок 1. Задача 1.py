0252aec4daeeb4a697a516e548b39ea93b47cc7e
Константин <89351929+Sachuhich@users.noreply.github.com>
2024-09-27 23:04:28 +0300

    Блок 1. Задача 1.py

--- Lesson 1 07.09.2024/Блок 1. Задача 1.py
+++ Lesson 1 07.09.2024/Блок 1. Задача 1.py
@@ -1,5 +1,10 @@
 # число десятков и единиц в 2значном числе
 
-num=int(input())
-if len(str(num))==2:print('Кол-во десятков: ',int(str(num)[0]),'\n','Кол-во единиц: ',int(str(num)[1]),sep=(''))
-else:print('Кол-во десятков: ',int(str(num)[1]),'\n','Кол-во единиц: ',int(str(num)[2]),sep=(''))
+
+
+def main():
+  num=int(input())
+  if len(str(num))==2:print('Кол-во десятков: ',int(str(num)[0]),'\n','Кол-во единиц: ',int(str(num)[1]),sep=(''))
+  else:print('Кол-во десятков: ',int(str(num)[1]),'\n','Кол-во единиц: ',int(str(num)[2]),sep=(''))
+if __name__ == '__main__':
+    main()
