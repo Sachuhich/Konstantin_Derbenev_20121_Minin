e8af1682f2f7fcdcfca91b76bed70bd805775ff4
Константин <89351929+Sachuhich@users.noreply.github.com>
2024-10-18 01:13:23 +0300

    Delete Lesson 1 07.09.2024 directory

--- Lesson 1 07.09.2024/Блок 1. Задача 1.py
+++
@@ -1,3 +0,0 @@
-num=int(input())
-if len(str(num))==2:print('Кол-во десятков: ',int(str(num)[0]),'\n','Кол-во единиц: ',int(str(num)[1]),sep=(''))
-else:print('Кол-во десятков: ',int(str(num)[1]),'\n','Кол-во единиц: ',int(str(num)[2]),sep=(''))
