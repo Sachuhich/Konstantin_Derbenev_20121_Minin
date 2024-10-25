e0e7fea537de0d17e9fb1cb2e99955df08fc5af0
Konstantin <89351929+Sachuhich@users.noreply.github.com>
2024-10-26 00:32:44 +0300

    Update Блок 4. Задача 1.py

--- Блок 4/Блок 4. Задача 1.py
+++ Блок 4/Блок 4. Задача 1.py
@@ -1,13 +1,24 @@
+'''
+Задача 1. С клавиатуры поступает строка.
+  Необходимо вывести самую длинную подстроку без повторных символов.
+Input: “qweasdfdqw"
+Output: "qweasd"
+Input: "aaaaaaa"
+Output: "a"
+Input: "prrker"
+Output: "rke"
+'''
+
 a = str(input())
 used,s,lst,end= '','',[],''
 for i in range(len(a)):
-  if a[i] not in used:
+  if a[i] not in used: # Выделяется подстрока
       used+=a[i]
       s+=a[i]
-  else:
+  else: # Добавление выделеной подстроки в lst, начало выделения новой подстроки
       lst.append([s, len(s)])
       s,used=a[i],a[i]
-if len(lst)>1:
+if len(lst)>1: # Если подстрок несколько, ищем самую длинную
   for i in range(len(lst)-1):
     if lst[i][1]>lst[i+1][1]:
       end=lst[i][0]
