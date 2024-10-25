458af4543e45a95744e5d13fe1485d86e0f59053
Konstantin <89351929+Sachuhich@users.noreply.github.com>
2024-10-26 00:07:26 +0300

    Update Блок 1. Задача 4.py

--- Блок 1/Блок 1. Задача 4.py
+++ Блок 1/Блок 1. Задача 4.py
@@ -1,5 +1,5 @@
 # 3знач число по цифрам через ,
 
 num=int(input())
-if len(str(num))==3:print(str(num)[0],str(num)[1],str(num)[2],sep=(','))
-else:print('-'+str(num)[1],str(num)[2],str(num)[3],sep=(','))
+if len(str(num))==3:print(str(num)[0],str(num)[1],str(num)[2],sep=(',')) # При условии, что число > 0
+else:print('-'+str(num)[1],str(num)[2],str(num)[3],sep=(',')) # При условии, что число < 0
