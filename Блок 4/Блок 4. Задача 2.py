ded72b6098f0f53d3bbc53e4f528abf53aefc9fe
Konstantin <89351929+Sachuhich@users.noreply.github.com>
2024-10-26 00:38:51 +0300

    Update Блок 4. Задача 2.py

--- Блок 4/Блок 4. Задача 2.py
+++ Блок 4/Блок 4. Задача 2.py
@@ -1,5 +1,17 @@
+'''
+Задание 2. С клавиатуры поступает строка.
+  Необходимо вывести строку, где порядок слов в противоположном направлении.
+  Первое слово с заглавной буквы, остальные с маленькой. МЕЖДУ словами только ОДИН пробел.
+Input: “hello world”
+Output: "Hello world"
+Input: it was cool وو
+Output: "Cool was it"
+Input: "good"
+Output: "Good"
+'''
+
 a=str(input()).lower()
 a=a.split()
 a.reverse()
-a[0] = a[0][0].upper() a[0][1:] 
+a[0] = a[0][0].upper()
 print(' '.join(a))
