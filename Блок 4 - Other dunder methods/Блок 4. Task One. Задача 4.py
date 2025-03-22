5a01506e8f9f01f4b2b45a41caa52117a3b0fd7c
Konstantin <89351929+Sachuhich@users.noreply.github.com>
2025-03-22 13:04:55 +0300

    Блок 4. Task One. Задача 4.py

--- Блок 4 - Other dunder methods/Блок 4. Task One. Задача 4.py
+++ Блок 4 - Other dunder methods/Блок 4. Task One. Задача 4.py
@@ -25,37 +25,3 @@ print(words.string)
 print(f"Число слов: {n}; первое слово: {first}")
 '''
 
-class WordString:
-    def __init__(self, string=''):
-        self.string=string
-
-    def __len__(self):
-        return len([word for word in self.string.split() if word])
-
-class WordString:
-    def __init__(self, string=''):
-        self._string = string
-
-    @property
-    def string(self):
-        return self._string
-
-    @string.setter
-    def string(self, value):
-        self._string = value
-
-    def __len__(self):
-        return len(self._string.split())
-
-    def __call__(self, indx):
-        words = self._string.split()
-        if 0 <= indx < len(words):
-            return words[indx]
-        return ''
-
-words = WordString()
-words.string = 'Python ООП'
-n = len(words)
-first = '' if n == 0 else words(0)
-print(words.string)
-print(f'Число слов: {n}; первое слово: {first}')
