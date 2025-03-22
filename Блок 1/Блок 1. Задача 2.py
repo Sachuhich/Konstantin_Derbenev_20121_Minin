d2d836a42660317f350b3580ceeaeda6417fb900
Konstantin <89351929+Sachuhich@users.noreply.github.com>
2025-03-22 07:06:12 +0300

    Delete Блок 1 directory

--- Блок 1/Блок 1. Задача 2.py
+++
@@ -1,5 +0,0 @@
-# Сумма цифр 2знач числа
-
-num=int(input())
-if len(str(num))==2:print('Сумма цифр:',int(str(num)[0])+int(str(num)[1])) # При условии, что число > 0
-else:print('Сумма цифр:',int(str(num)[2])+int(str(num)[1])) # При условии, что число < 0
--- Блок 1/Блок 1. Задача 4.py
+++
@@ -1,5 +0,0 @@
-# 3знач число по цифрам через ,
-
-num=int(input())
-if len(str(num))==3:print(str(num)[0],str(num)[1],str(num)[2],sep=(',')) # При условии, что число > 0
-else:print('-'+str(num)[1],str(num)[2],str(num)[3],sep=(',')) # При условии, что число < 0
